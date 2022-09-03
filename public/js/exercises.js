d39ab64a80ce1f18bcd236602c30d0fe60507a15
dionwdev <dionwilson.dev@gmail.com>
2022-09-02 22:17:37 -0400

    wod started in view and controller

--- public/js/exercises.js
+++ public/js/exercises.js
@@ -1,24 +1,22 @@
 const day1 = [
-    {
-       "workout1": "Pull-ups",
-       "workout2": "Squats",
-       "workout3": "Rows"
-    }
+       "Pull-ups",
+       "Squats",
+        "Rows"    
 ]
 
-const day2 = [
-    {
-       "workout1": "Chin-ups",
-       "workout2": "Lunges",
-       "workout3": "Sprints"
-    }
-]
+// const day2 = [
+//     {
+//        "workout1": "Chin-ups",
+//        "workout2": "Lunges",
+//        "workout3": "Sprints"
+//     }
+// ]
 
-const day3 = [
-    {
-       "workout1": "Shoulder-Press",
-       "workout2": "Dead-lifts",
-       "workout3": "Tread-mill"
-    }
-]
+// const day3 = [
+//     {
+//        "workout1": "Shoulder-Press",
+//        "workout2": "Dead-lifts",
+//        "workout3": "Tread-mill"
+//     }
+// ]
 
